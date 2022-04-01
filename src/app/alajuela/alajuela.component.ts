a0d7b88d0586a8e985f902f1af77cc2e8acebd98
Emman-n <enuneza937@gmail.com>
2022-04-01 13:47:37 -0600

    Provincia 1

---
+++ src/app/alajuela/alajuela.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alajuela',
+  templateUrl: './alajuela.component.html',
+  styleUrls: ['./alajuela.component.css']
+})
+export class AlajuelaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlajuelaComponent } from './alajuela/alajuela.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlajuelaComponent
   ],
   imports: [
     BrowserModule,
