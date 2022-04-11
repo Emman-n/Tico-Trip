4406c2454a424a7b9cf07607548baa09272c6754
dguerreror138 <danielguerrero98@outlook.com>
2022-04-10 22:41:56 -0600

    Login&Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrarseComponent } from './registrarse/registrarse.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistrarseComponent
   ],
   imports: [
     BrowserModule,
